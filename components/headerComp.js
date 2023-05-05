9cd6d6967d3ba57d59994661a574d5513c35ce42
Marcin Kosecki <marcin.kosecki@int.pl>
2023-05-05 16:07:20 +0200

    Create methods backToGalleria, displayGalleria, changeStye, notDisplayGalleria, removeComponents

--- components/headerComp.js
+++ components/headerComp.js
@@ -1,3 +1,5 @@
+import storage from "../tools/Storage.js";
+
 class Header extends HTMLElement {
   constructor() {
     super();
@@ -8,14 +10,45 @@ class Header extends HTMLElement {
   }
 
   connectedCallback() {
-      this.shadowRoot.innerHTML = `
+    this.shadowRoot.innerHTML = `
             <link rel="stylesheet" href="/styles/style.css">
             <img class="header__logo" src="/assets/shared/logo.svg" alt="logo">
             <p class="header__paragraph header__paragraph--start-stop" tabindex="0">start slideshow</p>
             `;
-    }    
+
+    const galleria = this.shadowRoot.querySelector(".header__logo");
+    galleria.addEventListener("click", () => this.backToGalleria(galleria));
+  }
+
+  backToGalleria(galleria) {
+    storage.clearStorage();
+    document.querySelector("cart-comp")
+      ? this.displayGalleria(galleria)
+      : this.notDisplayGalleria(galleria);
+  }
+
+  displayGalleria(galleria) {
+    this.removeComponents();
+    const main = document.querySelector("main");
+    const slidesComp = document.createElement("slides-comp");
+    main.appendChild(slidesComp);
+    this.changeStye(galleria)
+  }
+
+  changeStye(element) {
+    element.style.opacity = .2;
+    element.style.cursor = 'initial';
+    
+  }
+
+  notDisplayGalleria(galleria) {
+    galleria.removeEventListener("click", () => this.backToGalleria(galleria));
+  }
+
+  removeComponents() {
+    document.querySelector("footer-comp").remove();
+    document.querySelector("cart-comp").remove();
+  }
 }
 
 window.customElements.define("header-comp", Header);
-
-
