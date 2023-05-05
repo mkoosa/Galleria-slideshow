9c9cc2290f1c13ef2b98ef98e0618010db1d867f
Marcin Kosecki <marcin.kosecki@int.pl>
2023-05-05 07:32:06 +0200

    Create class Storage

---
+++ tools/Storage.js
@@ -0,0 +1,23 @@
+const KEY = "masterpiece"
+
+class Storage {
+    constructor(key) {
+      this.key = key
+    this.storage = localStorage;
+  }
+
+  setStorage(value) {
+    return this.storage.setItem(this.key, JSON.stringify(value));
+  }
+
+  getItemStorage() {
+    return JSON.parse(this.storage.getItem(this.key));
+  }
+
+  clearStorage() {
+    this.storage.removeItem(this.key);
+  }
+}
+
+const storage = new Storage(KEY);
+export default storage;
