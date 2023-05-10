bb1a0d49fa4f49a1da89390168177c58365e67b5
Marcin Kosecki <marcin.kosecki@int.pl>
2023-05-10 09:06:08 +0200

    Set addEventListener

--- components/slideComp.js
+++ components/slideComp.js
@@ -4,6 +4,8 @@ class Slide extends HTMLElement {
     const shadowRoot = this.attachShadow({ mode: "open" });
     const template = document.createElement("template");
     shadowRoot.appendChild(template.content.cloneNode(true));
+    // console.log(document.querySelector('slides-comp'));
+    this.slidesComp = document.querySelector('slides-comp')
   }
 
   set content(value) {
@@ -17,14 +19,17 @@ class Slide extends HTMLElement {
   connectedCallback() {
     this.shadowRoot.innerHTML = `
     <link rel="stylesheet" href="/styles/style.css">
-    <img class="slide__img" src="${this._content.images.thumbnail}" alt="starry-night">
+    <img class="slide__img" src="${this._content.images.thumbnail}" alt="${this._content.name}">
     <div class="slide__description">
     <h3 class="slide__title">${this._content.name}</h3>
     <p class="slide__painter">${this._content.artist.name}</p>
     </div>
     <div class="slide__gradient"></div>
     `;
+    this.setAttribute('tabindex', 0);
+    this.addEventListener("keypress", (e) =>  this.slidesComp.pressEnter(e))
   }
+  
 }
 
 window.customElements.define("slide-comp", Slide);
