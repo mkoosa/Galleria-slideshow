ac54a5f2581ac555a72e010922f187cb3bb211dd
Marcin Kosecki <marcin.kosecki@int.pl>
2023-05-18 21:46:22 +0200

    Update

--- tools/howler.js
+++ tools/howler.js
@@ -1,5 +1,5 @@
 let sound = new Howl({
-    // src: ['https://res.cloudinary.com/dwp8gkrp5/video/upload/v1684437755/Disco_mp3cut.net_pgh7rm.mp3'],
+    src: ['https://res.cloudinary.com/dwp8gkrp5/video/upload/v1684437755/Disco_mp3cut.net_pgh7rm.mp3'],
     volume:0.7,
     html5: true,
 });
